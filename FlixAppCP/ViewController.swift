d1b9d6651664991e10c7561469b8616400e1ae57
Antonella <avog001@fiu.edu>
2022-02-20 16:01:54 -0500

    Initial Commit

---
+++ FlixAppCP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixAppCP
+//
+//  Created by Antonella on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
