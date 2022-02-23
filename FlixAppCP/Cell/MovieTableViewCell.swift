7313fc61766b350cbb414fc2fcef7cc1edee2fa6
Antonella <avog001@fiu.edu>
2022-02-22 19:36:18 -0500

    fixed cell display issue

--- FlixAppCP/Cell/MovieTableViewCell.swift
+++ FlixAppCP/Cell/MovieTableViewCell.swift
@@ -2,15 +2,14 @@
 //  MovieTableViewCell.swift
 //  FlixAppCP
 //
-//  Created by Antonella on 2/21/22.
+//  Created by Antonella on 2/22/22.
 //
 
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
-    
+
+    @IBOutlet weak var moviePoster: UIImageView!
     @IBOutlet weak var movieLabel: UILabel!
     
-    
-
 }
--- FlixAppCP/ViewController.swift
+++ FlixAppCP/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     
     @IBOutlet weak var movieTableView: UITableView!
@@ -15,11 +15,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         movieTableView.dataSource = self
+        movieTableView.delegate = self
         
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200.0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
